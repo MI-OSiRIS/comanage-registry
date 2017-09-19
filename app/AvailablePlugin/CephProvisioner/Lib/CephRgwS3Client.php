da4f6bbbfe0fe0685f674cbec973f3259afaa113
Ben Meekhof <bmeekhof@users.noreply.github.com>
2017-09-19 11:31:59 -0400

    COManage Ceph provisioning plugin first version
    - Provisions data pools for new COU, renames if COU renamed.
    - Naming convention cou.Name.pooltype (fs, rgw, rados, rbd)
    - Does not ever delete data pools
    - Creates RGW zone placement keys referencing RGW data pool
    - Creates RGW zonegroup placement keys/tags referencing zone placement keys (tags are COU name)
    - If COU deleted zonegroup/zone placement info removed (pools not removed)
    - Creates RGW user named with pattern CoPerson identifier_couname.
    - Assigns data placement tags and default placement to users according to COU
    - Removes RGW users if they are not in COU identified by suffix, COU does not exist, or no person with identifier exists
    - RGW usernames with _ are reserved for this plugin only so it can figure out what to verify and cleanup without removing users not managed by plugin
    - Designed to work in coordination with CephRgwLdapTokenProvisioner and modified CoServiceToken plugins to provision users and passwords to LDAP OU for authentication

---
+++ app/AvailablePlugin/CephProvisioner/Lib/CephRgwS3Client.php
@@ -0,0 +1,7 @@
+<?php
+
+// http://docs.ceph.com/docs/master/radosgw/s3/php/
+// http://docs.aws.amazon.com/aws-sdk-php/v3/guide/getting-started/basic-usage.html
+
+require '/usr/share/php/Aws/autoload.php';
+
